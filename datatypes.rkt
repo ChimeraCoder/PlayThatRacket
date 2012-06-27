3cba62739bf10297d48ddc93048c9ae16174961b
aditya <dev@chimeracoder.net>
2012-06-26 22:54:17 -0400

    Add function for testing that a value is a music-cons

--- datatypes.rkt
+++ datatypes.rkt
@@ -9,10 +9,15 @@
   ;;    1) A numeric (integral) value (in which case the units are milliseconds)
   ;;    2) Another music-cons
   ;;    3) Null (which is a special case of music-cons)
-  (if (ormap number? (list pitch duration))
-    (raise-user-error "music-cons called with numeric arguments")
+  (if (not (andmap procedure? (list pitch duration)))
+    (raise-user-error "music-cons called with at least one non-procedure argument")
     (quasiquote ((unquote pitch) (unquote duration)))))
 
+(define (music-cons? val1)
+  (andmap (list 
+            (equal? (length val1) 2)  ;;Length of a cons must be 2
+            (or (procedure? (car val1))
+                (music-cons? (car val1)))))) ;;Each cell of a cons must be either a procedure or another music-cons
 
 (define (pitch-function pitch-number)
     ;;Theoretically, this is redundant and equivalent to duration-function, but it is a useful abstraction when starting out
