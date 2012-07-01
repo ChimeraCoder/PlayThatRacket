f363098bb64517e7840fbb4f97b7f8ec09228f30
aditya <dev@chimeracoder.net>
2012-06-30 20:56:49 -0400

    Use typed/racket for make test, and move test file to tests/ subdir

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ clean:
 	rm -rf "compiled/" "*.swp"
 
 test:
-	racket datatypes-test.rkt
+	racket tests/datatypes-test.rkt -I typed/racket
--- tests/datatypes-test.rkt
+++ tests/datatypes-test.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 (require rackunit
-         "datatypes.rkt")
+         "../datatypes.rkt")
 
 ;; TODO: Add assertions.
 ;;(check-exn
