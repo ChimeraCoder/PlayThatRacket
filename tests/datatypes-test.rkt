b857afbb74197d0826d10dd7ebbb1fc22bd64813
aditya <dev@chimeracoder.net>
2012-07-09 22:29:45 -0400

    Add explicit test for join-phrases function

--- tests/datatypes-test.rkt
+++ tests/datatypes-test.rkt
@@ -13,7 +13,8 @@
 
 (define phrase1 (list (note 1 4) (note 2 4) (note 3 4)))
 (define phrase2 (list (note 5 4) (note 6 4) (note 7 4)))
-(check-equal? (join-phrases phrase1 phrase2) (append phrase1 phrase2) "join-phrase test failed")
+(check-equal? (join-phrases phrase1 phrase2) (append phrase1 phrase2) "Implicit join-phrase test failed")
+(check-equal? (join-phrases phrase1 phrase2) (list (note 1 4) (note 2 4) (note 3 4)  (note 5 4) (note 6 4) (note 7 4))  "Explicit join-phrase test failed")
 
 
 (define A (note 440 500))
