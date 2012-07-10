60529ec76bcad466194128d762a0d5c911c35b21
aditya <dev@chimeracoder.net>
2012-07-09 22:18:33 -0400

    Add tests for semitone-up and semitone-down functions

--- tests/datatypes-test.rkt
+++ tests/datatypes-test.rkt
@@ -14,3 +14,18 @@
 (define phrase1 (list (note 1 4) (note 2 4) (note 3 4)))
 (define phrase2 (list (note 5 4) (note 6 4) (note 7 4)))
 (check-equal? (join-phrases phrase1 phrase2) (append phrase1 phrase2) "join-phrase test failed")
+
+
+(define A (note 440 500))
+(define A# (note (round (inexact->exact 466.16)) 500))
+(define Ab (note (round (inexact->exact 415.486)) 500))
+
+
+;;Check that semitone-{up,down} adjusts pitch to within 1Hz of the correct value
+(check-= (note-pitch (semitone-up A)) (note-pitch A#) 1 "Semitone up check failed")
+(check-= (note-pitch (semitone-down A)) (note-pitch Ab) 1 "Semitone down check failed")
+
+
+
+
+
