2f09f187a695bac6030504b0c700475a73683ea5
Matthias Bundai <zenceph@gmail.com>
2012-07-19 19:55:15 -0500

    Convert delta times to variable length byte encoding that midi uses.

--- compile-midi.rkt
+++ compile-midi.rkt
@@ -10,5 +10,36 @@
    (define (collapse-to-deltas lst)
      (cons (car lst) (calculate-deltas lst)))
 
+   ;; returns the binary bits backwards
+   (define (delta-to-binary n)
+     (if (>= n 1)
+            (cons (if (even? n)
+                    0
+                    1)
+            (delta-to-binary
+              (quotient n 2)))
+            null))
+
+   (define (binary-to-midi-encoding b start)
+     (let ([bit (if start 0 1)])
+     (if (> (length b) 7)
+       (append 
+               (binary-to-midi-encoding (drop b 7) #f)
+               (cons bit (reverse (take b 7)))
+               )
+       (cons bit (pad-binary (reverse b))))
+     ))
+
+   (define (delta-to-midi delta)
+     (binary-to-midi-encoding (delta-to-binary delta) #t))
+
+   (define (pad-binary binary)
+     (if (< (length binary) 7)
+       (pad-binary (cons 0 binary))
+       binary)
+     )
+
+
+
    (provide (all-defined-out))
 )
