6db9b0b7ffa07e6d3dc5696a446bf66cad114ab4
Matthias Bundai <zenceph@gmail.com>
2012-07-21 10:19:36 -0500

    Midi event byte conversions, (note-on)

--- compile-midi.rkt
+++ compile-midi.rkt
@@ -11,14 +11,14 @@
      (cons (car lst) (calculate-deltas lst)))
 
    ;; returns the binary bits backwards
-   (define (delta-to-binary n)
+   (define (num-to-binary n)
      (if (= n 0)
        0
         (if (>= n 1)
                (cons (if (even? n)
                        0
                        1)
-               (delta-to-binary
+               (num-to-binary
                  (quotient n 2)))
                null)))
 
@@ -29,23 +29,46 @@
                (binary-to-midi-encoding (drop b 7) #f)
                (cons bit (reverse (take b 7)))
                )
-       (cons bit (pad-binary (reverse b))))
+       (cons bit (pad-binary (reverse b) 7)))
      ))
 
    (define (delta-to-midi delta)
-     (binary-to-midi-encoding (delta-to-binary delta) #t))
+     (binary-to-midi-encoding (num-to-binary delta) #t))
 
-   (define (pad-binary binary)
-     (if (< (length binary) 7)
-       (pad-binary (cons 0 binary))
+   (define (pad-binary binary n)
+     (if (< (length binary) n)
+       (pad-binary (cons 0 binary) n)
        binary)
      )
+
+   (define (midi-event-to-nibble event)
+     (match event
+            ['note-off '(1 0 0 0)]
+            ['note-on '(1 0 0 1)]
+            ['key-after-touch '(1 0 1 0)]
+            ['control-change '(1 0 1 1)]
+            ['program-change '(1 1 0 0)]
+            ['channel-after-touch '(1 1 0 1)]
+            ['pitch-wheel-change '(1 1 1 0)]
+            ))
+
+   (define (midi-meta-event-to-byte event)
+     (match event
+            ['set-tempo '(0 1 0 1 0 0 0 1)]
+            ['time-signature '(0 1 0 1 1 0 0 0)]
+            ))
 
    (define (bit-list-to-chars bit-list)
      (if (null? bit-list)
        null
        (cons (foldl + 0 (map (lambda (a b) (* a b)) (take bit-list 4) '(8 4 2 1))) 
-             (bit-list-to-chars (drop bit-list 4))))
+             (bit-list-to-chars (drop bit-list 4)))))
+
+   (define (note-on channel note velocity)
+     (append (midi-event-to-nibble 'note-on) 
+             (pad-binary (num-to-binary channel) 4) 
+             (pad-binary (num-to-binary note) 2) 
+             (pad-binary (num-to-binary velocity) 2)))
 
 
    (define (header-chunk) '(0x4d 0x54 0x68 0x64 0x00 0x00 0x00 0x06))
