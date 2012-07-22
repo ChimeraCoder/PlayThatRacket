4ae5e9c09f303a0446c09038d87e961cd436a437
aditya <dev@chimeracoder.net>
2012-07-21 21:11:57 -0400

    Create supermap modue which provides map/skip function

--- supermap.rkt
+++ supermap.rkt
@@ -1,48 +1,21 @@
+(module supermap racket
 
-(define (mapskip desired-function lst)
-  (let* ((toggler (make-toggler true)))  ;; switcher is a function that switches to the identity fun
-    (map (lambda (x) (apply-if (toggler) desired-function x)) lst)))
-    
+ (define (map/skip desired-function lst (skip-first #t))
+  (let* ((toggler (make-toggler #t)))  ;; switcher is a function that switches to the identity fun
+   (map (lambda (x) (apply-if (toggler) desired-function x)) lst)))
 
+ (define (apply-if predicate desired-function x)
+  (if predicate
+   (desired-function x)
+   x))
 
-  (lambda (x start)
-    (define skip-current start)
-
-(define (make-toggler desired-function)
-  (let ([apply? true])
-    (lambda ()
-      (let ((result (possibly-apply desired-function apply?)))
-        (set! apply? (not apply?))
-        result))))
-
-(define (apply-if predicate desired-function x)
-    (if predicate
-      (desired-function x)
-      x))
-
-
-(define contrarian
-  (let* ((response true))
-     (lambda ()
-       (set! response (not response))
-       response)))
-
-;;A toggler returns the opposite response each time you call it
-(define (make-toggler starting-value)
+ ;;A toggler returns the opposite response each time you call it
+ (define (make-toggler starting-value)
   (lambda ()
-    (set! starting-value (not starting-value))
-    starting-value))
-
-
-
-(define (foo entire-tree)
-  (let* ((length-declr (length (cdr tree)))
-         (stuff-for-header (make-header length-declr bar baz)))
-     (print-result stuff-for-header (cdr tree))))
-
-
-
-
+   (set! starting-value (not starting-value))
+   starting-value))
+ 
+ (provide map/skip))
 
 
 
