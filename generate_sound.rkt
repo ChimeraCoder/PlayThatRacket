fa9b4e17539cf0c5ec4a9ccf984858ee99bdccaa
aditya <dev@chimeracoder.net>
2012-12-15 15:28:11 -0500

    Add example of plaing a chromatic scale

--- generate_sound.rkt
+++ generate_sound.rkt
@@ -1,41 +1,44 @@
-#lang racket
+#lang typed/racket/no-check
 
 (require (planet clements/rsound))
-    (require "datatypes.rkt")
+(require "datatypes.rkt")
+(require "scale.rkt")
 
-    (define counter/sig
-     (network ()
-      [counter (+ 1 (prev counter 0))]))
- 
+(: note->sound (case -> (note -> rsound)
+                        (note -> Number rsound)))
 ;;Given a pitch (in Hz), a duration (in number frames), and volume (between 0 and 1), return an rsound s16vector that can be played with the #'play function
-(define (create-sound note [volume 0.1]) ;TODO parameterize volume
+(define (note->sound note [volume 0.1]) ;TODO parameterize volume
  (let* ((pitch (note-pitch note))
         (duration (ms->frames (note-duration note)))
         (sig1
-        (network ()
-         [a (sine-wave pitch)]
-         [out (* volume a)])))
+         (network ()
+          [a (sine-wave pitch)]
+          [out (* volume a)])))
 
   (signal->rsound duration sig1)))
-
 
 ;;TODO actually time this conversion to see what's correct
 ;:Right now, we're assuming 1 second is 44100 frames
+(: ms->frames (Integer -> Integer))
 (define (ms->frames milliseconds)
   (* milliseconds (/ 44100 1000)))
 
-(define (note->signal note [volume 0.1])
- (let* ((pitch (note-pitch note))
-       (duration (ms->frames (note-duration note)))
-       (sig1
-        (network ()
-         [a (sine-wave pitch)]
-         [out (* volume a)])))
-  (signal->rsound duration sig1)))
+(: play-notes (Listof note -> ))
+(define (play-notes notes)
+  (play (rs-append* (map note->sound notes))))
+
 
 ;;Play A440 for 44100 frames at .1 of the maximum volume
-;(play (rs-append* (list (create-sound 440 44100 )(create-sound 540 44100 .1))))
-(play (rs-append* (list (create-sound (note 440 500)) (create-sound (note 540 500) .1))))
+;(play (rs-append* (list (note->sound 440 44100 )(note->sound 540 44100 .1))))
+;(play (rs-append* (list (note->sound (note 440 500)) (note->sound (note 540 500) .1))))
+
+(define chromatic-scale-4th (list C C♯ C# D♭ Db D D♯ D# E♭ Eb E F F♯ F# G♭ Gb G G♯ G# A♭ Ab A A♯ A# B♭ Bb B))
+(play-notes chromatic-scale-4th)    
+
+
+
+
+;(play-notes (list (note 440 500) (note 540 500)))
 
 ;;If you don't sleep, the program will exit before it actually gets a chance to play
-(sleep 200)
+(sleep 2000)
