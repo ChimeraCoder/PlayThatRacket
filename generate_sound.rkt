35dad4e4b35775470e4a6d0a5c1c3dfbee36e576
aditya <dev@chimeracoder.net>
2012-12-15 17:18:23 -0500

    Compose Twinkle Twinkle, Little Star as a modular composition

--- generate_sound.rkt
+++ generate_sound.rkt
@@ -7,8 +7,9 @@
 
 (: note->sound (case -> (note -> rsound)
                         (note -> Number rsound)))
+
 ;;Given a pitch (in Hz), a duration (in number frames), and volume (between 0 and 1), return an rsound s16vector that can be played with the #'play function
-(define (note->sound note [volume 0.1]) ;TODO parameterize volume
+(define (note->sound note [volume 0.3]) ;TODO parameterize volume
  (let* ((pitch (note-pitch note))
         (duration (ms->frames (note-duration note)))
         (sig1
@@ -17,6 +18,14 @@
           [out (* volume a)])))
 
   (signal->rsound duration sig1)))
+
+
+;;Convert a note directly into an rsound, without processing as a signal first
+(define (note->tone note [volume 0.1])
+  (make-tone (note-pitch note ) volume (ms->frames (note-duration note))))
+
+
+
 
 ;;TODO actually time this conversion to see what's correct
 ;:Right now, we're assuming 1 second is 44100 frames
@@ -29,14 +38,28 @@
   (play (rs-append* (map note->sound notes))))
 
 
-;;Play A440 for 44100 frames at .1 of the maximum volume
-;(play (rs-append* (list (note->sound 440 44100 )(note->sound 540 44100 .1))))
-;(play (rs-append* (list (note->sound (note 440 500)) (note->sound (note 540 500) .1))))
+
+
+
+
 
 (define chromatic-scale-4th (list C C♯ C# D♭ Db D D♯ D# E♭ Eb E F F♯ F# G♭ Gb G G♯ G# A♭ Ab A A♯ A# B♭ Bb B))
 
+(define first-phrase (list A A E5 E5 F5# F5# E5))
+(define second-phrase (list D5 D5 C5# C5# B B A))
+(define third-phrase (list E5 E5 D5 D5 C5# C5# B))
+(define rest (note 0 500)) ;;temporary hack
+
 (define chromatic-scale-5th (raise-all-octave chromatic-scale-4th))
 
+(define (mozart-form a b c)
+  (list a rest b rest c rest c rest a rest b))
+
+;(play-notes chromatic-scale-5th)
+
+
+;(play-notes (flatten (list first-phrase rest second-phrase rest third-phrase rest third-phrase rest first-phrase rest second-phrase)))
+(play-notes (flatten (mozart-form first-phrase second-phrase third-phrase)))
 
 
 
