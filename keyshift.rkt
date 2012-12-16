ec1f95b0f5b5ffd86cd6fc504c259fe8505a0ee6
aditya <dev@chimeracoder.net>
2012-12-15 19:41:40 -0500

    Add some type annotations to keyshift.rkt, though still no static checking

--- keyshift.rkt
+++ keyshift.rkt
@@ -4,6 +4,7 @@
    (require "basic-ops-untyped.rkt")
    (require "scale.rkt")
 
+  (: transform-notes (Phrase -> (Phrase -> Phrase) -> Phrase))
   (define (transform-notes input func)
     ;;TODO replace with pattern-matchin
     (cond 
@@ -11,18 +12,21 @@
       [(list? input) (map (lambda (in) (transform-notes in func)) input)]));(func input)
 
   ;:Change the pitch of a note but keep the duration
+  (: change-pitch (note -> note -> note))
   (define (change-pitch nt target-note)
     (note (note-pitch target-note) (note-duration nt)))
 
+  (: replace-A-with-D (Phrase -> Phrase))
   (define (replace-A-with-D nts)
-   (transform-notes nts (lambda (nt) (if (A? nt)
+   (transform-notes nts (lambda: ([nt : Phrase]) (if (A? nt)
                                       (note (note-pitch D) (note-duration nt))
                                       nt))))
   
   ;;Table is a list of pairs
   ;;Returns a single function
   (define (compose-functions-from-table table)
-    (apply compose1 (map (lambda (pair) (lambda (nt)
+    (apply compose1 (map (lambda: ([pair : (Pairof (note -> Boolean) (note -> note))]) 
+                            (lambda: ([nt : note])
                                    (if ((car pair) nt) ;;If the first function is true
                                      ((cadr pair) nt) ;;then apply the second fnction
                                      nt))) ;;otherwise return the note unchanged
