c9f012c6177d9c0a18df2fd85ea281b9ae0e6b18
aditya <dev@chimeracoder.net>
2013-01-09 22:55:11 -0500

    Add basic file to demonstrate decompilation

---
+++ decompile.rkt
@@ -0,0 +1,10 @@
+ compiler/decompile)
+(require compiler/zo-parse)
+
+
+(define fin (open-input-file "compiled/datatypes_rkt.zo" #:mode 'binary))
+
+(define bytecode (zo-parse fin))
+
+(define source (decompile bytecode))
+
