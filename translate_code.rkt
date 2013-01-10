f9fe964f10454b185f3cb7546dc82054f0dd3ffb
aditya <dev@chimeracoder.net>
2013-01-09 23:15:57 -0500

    Remove cruft

--- translate_code.rkt
+++ translate_code.rkt
@@ -24,26 +24,5 @@
     [(list-rest a b ...d) (cons (note (min a MAX_PITCH) (min b MAX_DURATION)) (make-notes (cddr nums)))]
     [(list a) (note (min a MAX_PITCH) 5)]
     [_ '()]))
-
-#|
-  (match tree
-    [(list 'note x y) (note x y)]
-    [(? list?) (flatten (list 
-                    (filter note? tree)
-                    (ensure-note (filter (lambda (x) (not (note? x))) tree))
-    (flatten (map translate tree))]
-    ;;[(symbol? tree) (string-length (symbol->string tree))]
-    [_ (note 440 500)]))
-    |#
-   
-   #|
-  (if (not (list? tree))  ;Check if this is a single element
-    (if (symbol? tree)
-      (string-length (symbol->string tree))
-      1) ;; Default
-    (let ([elements-tr (flatten (map translate tree))])
-      (if (andmap note? elements-tr)
-        elements-tr
-        (note 440 500)))))|#
-          
+      
 (provide (all-defined-out))      
