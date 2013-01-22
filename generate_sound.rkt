03f777800e46858057aa44afaefbf3b9a79aa7b8
aditya <dev@chimeracoder.net>
2013-01-21 22:52:58 -0500

    Move Twinkle Twinkle, Little Star to its own file

--- generate_sound.rkt
+++ generate_sound.rkt
@@ -19,16 +19,6 @@
 (define (mozart-form a b c)
   (list a rest b rest c rest c rest a rest b))
 
-;;Define each of the three phrases in "Twinkle Twinkle, Little Star"
-(define first-phrase (list A A E5 E5 F5♯ F5♯ E5))
-(define second-phrase (list D5 D5 C5♯ C5♯ B B A))
-(define third-phrase (list E5 E5 D5 D5 C5♯ C5♯ B))
-(define rest (list (note 0 500))) ;;temporary hack
-
-(define twinkle-twinkle-little-star (mozart-form first-phrase second-phrase third-phrase))
-
-
-
 ;;A lot of modern and/or experimental music mixes both fixed components and components that are defined at 'performance-time' by the performer and/or audience.
 ;;No two performances are the same!
 
---
+++ twinkle_twinkle_little_star.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require "rsound.rkt")
+(require "basic-ops-typed.rkt")
+(require "scale.rkt")
+(require "keyshift.rkt")
+(require "datatypes.rkt")
+
+;;The song "Twinkle Twinkle, Little Star" has a common structure,
+;;and we might want to use that structure later to compose other songs too.
+(define (mozart-form a b c)
+  (list a rest b rest c rest c rest a rest b))
+
+;;Define each of the three phrases in "Twinkle Twinkle, Little Star"
+(define first-phrase (list A A E5 E5 F5♯ F5♯ E5))
+(define second-phrase (list D5 D5 C5♯ C5♯ B B A))
+(define third-phrase (list E5 E5 D5 D5 C5♯ C5♯ B))
+(define rest (list (note 0 500))) ;;temporary hack
+
+(define twinkle-twinkle-little-star (mozart-form first-phrase second-phrase third-phrase))
+
+(play-notes (flatten twinkle-twinkle-little-star))
+(sleep 2000)
+
+(provide (all-defined-out))
